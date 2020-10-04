e0d1bb5d5c8053aa5bcccadae7c842ffd0ba7ede
Jacob Nelson <NelsonJacob1988@gmail.com>
2020-10-04 12:01:43 -0500

    uv index displays properly

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ var firstURL = "https://api.openweathermap.org/data/2.5/weather?q=Austin&units=i
 function displayCityName() {
     var cityName = $(this).attr("data-name");
     var displayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + API_KEY;
-  $.ajax({
-      url: displayURL,
-      method: "GET"
-  }).then(function(response){
-    $(".city").html("<h1>" + response.name + " weather details</h1>");
-    $(".temp").text("Temperature: " + response.main.temp + "F");
-    $(".humidity").text("Humidity: " + response.main.humidity + "%");
-    $(".wind").text("Wind speed: " + response.wind.speed + "s");
-  })
+    $.ajax({
+        url: displayURL,
+        method: "GET"
+    }).then(function (response) {
+        $(".city").html("<h1>" + response.name + " weather details</h1>");
+        $(".temp").text("Temperature: " + response.main.temp + "F");
+        $(".humidity").text("Humidity: " + response.main.humidity + "%");
+        $(".wind").text("Wind speed: " + response.wind.speed + "s");
+        // provides UV data
+        var UVurl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + API_KEY;
+
+        $.ajax({
+            url: UVurl,
+            method: "GET"
+        }).then(function (response) {
+            $(".uv").text("UV index: " + response.value);
+        });
+    })
 }
 // renders a button for each city in the "cities" array
 function renderButtons() {
@@ -30,16 +39,25 @@ function renderButtons() {
 
 // provides weather details for Austin when you first load the page
 function renderWeather() {
-$.ajax({
-    url: firstURL,
-    method: "GET"
-}).then(function (response) {
-    console.log(response);
-    $(".city").html("<h1>" + response.name + " weather details</h1>");
-    $(".temp").text("Temperature: " + response.main.temp + "F");
-    $(".humidity").text("Humidity: " + response.main.humidity + "%");
-    $(".wind").text("Wind speed: " + response.wind.speed + "s");
-});
+    $.ajax({
+        url: firstURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        $(".city").html("<h1>" + response.name + " weather details</h1>");
+        $(".temp").text("Temperature: " + response.main.temp + "F");
+        $(".humidity").text("Humidity: " + response.main.humidity + "%");
+        $(".wind").text("Wind speed: " + response.wind.speed + "s");
+        // provides UV data
+        var UVurl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + API_KEY;
+
+        $.ajax({
+            url: UVurl,
+            method: "GET"
+        }).then(function (response) {
+            $(".uv").text("UV index: " + response.value);
+        });
+    });
 }
 // provides weather deatils for city user searches
 $("#find-city").on("click", function (event) {
@@ -65,27 +83,16 @@ $("#find-city").on("click", function (event) {
             method: "GET"
         }).then(function (response) {
             $(".uv").text("UV index: " + response.value);
-
-
-            // var forcastURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + 30.27 + "&lon=" + -94.74 + "&appid=" + API_KEY;
-            // // var forcastURL = "http://api.openweathermap.org/data/2.5/forecast?id=" + response.coord.id + "&appid=" + API_KEY;
-            // $.ajax({
-            //     url: forcastURL,
-            //     method: "GET"
-            // }).then(function(response) {
-            //     console.log(response);
-            //     $(".forcast1").text(response.list[0])
-            // })
-        })
+        });
     });
 });
 // adds button for city user searches
 $("#find-city").on("click", function (event) {
-    
+
     event.preventDefault();
     var city = $("#city-input").val().trim();
     cities.push(city);
-    $("#city-input").val("")
+    $("#city-input").val("");
     renderButtons();
 });
 
