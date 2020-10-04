e8f975589aac6c83bcac50d2a9077606146dc8ed
Jacob Nelson <NelsonJacob1988@gmail.com>
2020-10-04 15:34:23 -0500

    5 day forecast displays user city

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ function displayCityName() {
                 $(".uv").attr("class", "uv low");
             }
         });
+        
     })
 }
 // renders a button for each city in the "cities" array
@@ -73,6 +74,7 @@ function renderWeather() {
                 $(".uv").attr("class", "uv low");
             }
         });
+        
     });
 }
 // provides weather deatils for city user searches
@@ -108,6 +110,29 @@ $("#find-city").on("click", function (event) {
                 $(".uv").attr("class", "uv low");
             }
         });
+        var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + userCity + "&units=imperial&appid=" + API_KEY
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            $(".date0").text(response.list[5].dt_txt);
+            $(".date1").text(response.list[13].dt_txt);
+            $(".date2").text(response.list[21].dt_txt);
+            $(".date3").text(response.list[29].dt_txt);
+            $(".date4").text(response.list[37].dt_txt);
+            $(".forecastTemp0").text("Temp: " + response.list[5].main.temp);
+            $(".forecastTemp1").text("Temp: " + response.list[13].main.temp);
+            $(".forecastTemp2").text("Temp: " + response.list[21].main.temp);
+            $(".forecastTemp3").text("Temp: " + response.list[29].main.temp);
+            $(".forecastTemp4").text("Temp: " + response.list[37].main.temp);
+            $(".forecastHum0").text("Humidity: " + response.list[5].main.humidity + "%");
+            $(".forecastHum1").text("Humidity: " + response.list[13].main.humidity + "%");
+            $(".forecastHum2").text("Humidity: " + response.list[21].main.humidity + "%");
+            $(".forecastHum3").text("Humidity: " + response.list[29].main.humidity + "%");
+            $(".forecastHum4").text("Humidity: " + response.list[37].main.humidity + "%");
+            
+        });
     });
 });
 // adds button for city user searches
@@ -120,33 +145,7 @@ $("#find-city").on("click", function (event) {
     renderButtons();
 });
 
-// $(".city-btn").on("click", function (){
-//     $(".city").html("<h1>" + response.name + " weather details</h1>");
-//     $(".temp").text("Temperature: " + response.main.temp + "F");
-//     $(".humidity").text("Humidity: " + response.main.humidity + "%");
-//     $(".wind").text("Wind speed: " + response.wind.speed + "s");
-// })
-
 $(document).on("click", ".city-btn", displayCityName);
 
 renderWeather();
 renderButtons();
-
-
-
-
-
-
-
-// $.ajax({
-//   url: url,
-//   method: "GET"
-// }).then(function(response){
-//   console.log(response);
-//   $(".city").html("<h1>" + response.name + " weather details</h1>");
-//   $(".wind").text("Wind speed: " + response.wind.speed + "s");
-//   $(".humidity").text("Humidity: " + response.main.humidity + "%");
-//   $(".temp").text("Temperature: " + response.main.temp + "F")
-// }); 
-
-// "http://api.openweathermap.org/data/2.5/uvi?lat=" + response.coord.lat + "&lon=" + response.coord.lon + "&appid=" + API_KEY
